d763d6e6edec9910af3f39deb6eb03ffd0fb1d00
kiki <k.kazaneko@gmail.com>
2022-11-29 13:47:29 +0900

    Initial Commit

---
+++ NoteAppCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteAppCoreData
+//
+//  Created by 久島風音 on 2022/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
